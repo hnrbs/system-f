a7f8b2edda2d021b24ea904ec6b891ad489b8691
henri <henri1i.dev@gmail.com>
2023-09-04 17:52:10 -0300

    feat: implement pstlc adding types to lambda calculus

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,107 @@
+use std::fmt::Display;
+
 use im::hashmap::HashMap;
+
+#[derive(Debug, Clone, PartialEq)]
+enum Type {
+    Closure { param: Box<Type>, body: Box<Type> },
+    Int,
+    String,
+}
+
+impl Type {
+    fn name(&self) -> String {
+        match self {
+            Type::Closure { param, body } => {
+                let param = (*param).name();
+                let body = (*body).name();
+
+                format!("closure<{} -> {}>", param, body)
+            }
+            Type::Int => String::from("int"),
+            Type::String => String::from("string"),
+        }
+    }
+}
 
 #[derive(Debug, Clone)]
 enum Expr {
     Int(i64),
     String(String),
     Var(String),
-    Abs { param: String, body: Box<Expr> },
-    App { arg: Box<Expr>, abs: Box<Expr> },
+    Abs {
+        param: String,
+        param_type: Type,
+        body: Box<Expr>,
+    },
+    App {
+        arg: Box<Expr>,
+        abs: Box<Expr>,
+    },
+}
+
+type TypeContext = HashMap<String, Type>;
+
+fn infer(expr: Expr, context: TypeContext) -> Type {
+    match expr {
+        Expr::Int(_int) => Type::Int,
+        Expr::String(_str) => Type::String,
+        Expr::Var(var) => context
+            .get(&var)
+            .expect(&format! {"type error: unbound variable {var}"})
+            .clone(),
+        Expr::Abs {
+            param,
+            param_type,
+            body,
+        } => {
+            let context = context.update(param, param_type.clone());
+
+            let body = infer(*body, context);
+            Type::Closure {
+                param: Box::new(param_type),
+                body: Box::new(body),
+            }
+        }
+        Expr::App { arg, abs } => {
+            let arg = infer(*arg, context.clone());
+
+            match infer(*abs, context) {
+                Type::Closure { param, body } => match *param == arg {
+                    true => *body,
+                    false => panic!("expecting type {}. {} given", (*param).name(), arg.name()),
+                },
+                typ => panic!("type {} cannot be used as a closure", typ.name()),
+            }
+        }
+    }
 }
 
 #[derive(Clone, Debug)]
 enum Value {
-    VClosure {
+    Closure {
         param: String,
         body: Expr,
-        context: Context,
+        context: ValueContext,
     },
-    VInt(i64),
-    VString(String),
-    VNative(fn(Box<Value>) -> Box<Value>),
+    Int(i64),
+    String(String),
+    Native(fn(Box<Value>) -> Box<Value>),
 }
 
-type Context = HashMap<String, Value>;
+type ValueContext = HashMap<String, Value>;
 
-fn eval(expr: Expr, context: Context) -> Value {
+fn eval(expr: Expr, context: ValueContext) -> Value {
     match expr {
         Expr::Var(var) => context
             .get(&var)
             .expect(&format!("unbound variable: {}", var))
             .clone(),
-        Expr::Abs { param, body } => Value::VClosure {
+        Expr::Abs {
+            param,
+            param_type: _,
+            body,
+        } => Value::Closure {
             param,
             body: *body,
             context,
@@ -38,7 +110,7 @@ fn eval(expr: Expr, context: Context) -> Value {
             let arg = eval(*arg, context.clone());
 
             match eval(*abs, context) {
-                Value::VClosure {
+                Value::Closure {
                     param,
                     body,
                     context,
@@ -47,38 +119,43 @@ fn eval(expr: Expr, context: Context) -> Value {
 
                     eval(body, context)
                 }
-                Value::VNative(native) => *(native(Box::new(arg))),
-                Value::VInt(_value) => panic!(),
-                Value::VString(_value) => panic!(),
+                Value::Native(native) => *(native(Box::new(arg))),
+                Value::Int(_value) => panic!(),
+                Value::String(_value) => panic!(),
             }
         }
-        Expr::Int(int) => Value::VInt(int),
-        Expr::String(string) => Value::VString(string),
+        Expr::Int(int) => Value::Int(int),
+        Expr::String(string) => Value::String(string),
     }
 }
 
 fn main() {
-    let print = Value::VNative(|value: Box<Value>| {
+    let type_context = TypeContext::new();
+
+    let t_print = Type::Closure {
+        param: Box::new(Type::Int),
+        body: Box::new(Type::Int),
+    };
+
+    let type_context = type_context.update(String::from("print"), t_print);
+
+    let v_print = Value::Native(|value: Box<Value>| {
         match *value.clone() {
-            Value::VInt(value) => println!("{}", value),
-            Value::VString(value) => println!("{}", value),
-            Value::VNative(_value) => panic!("natives cannot be printed"),
-            Value::VClosure {
-                param: _,
-                body: _,
-                context: _,
-            } => panic!("closures cannot be printed"),
+            Value::Int(value) => println!("{}", value),
+            _ => panic!(),
         };
 
         value
     });
 
-    let context = Context::new().update(String::from("print"), print);
+    let value_context = ValueContext::new().update(String::from("print"), v_print);
 
     let ast = Expr::App {
         abs: Box::new(Expr::Var(String::from("print"))),
         arg: Box::new(Expr::String("Hello, World!".to_owned())),
     };
 
-    let _result = eval(ast, context);
+    let _result_type = infer(ast.clone(), type_context);
+
+    let _result_value = eval(ast, value_context);
 }
